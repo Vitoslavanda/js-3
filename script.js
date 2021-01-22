ad33a28cc59f202119bacef426b9e79c84fe902a
Vitoslavanda <vitka.10165@gmail.com>
2021-01-22 19:26:53 +0200

    last

--- script.js
+++ script.js
@@ -1,5 +1,305 @@
-let a = 2,
-const s = 'back';
+// -- створити об'єкт (не меньше 5ти властивостей) який описує
+// - собаку
+// let dog = {
+//     Name : 'Cesar',
+//     age : 4,
+//     color : 'Brown',
+//     breed : 'Buldog',
+//     size : 'Big' 
+// }
+
+
+// // - людину
+// let person = {
+//     Name : 'Alex',
+//     age : 25,
+//     Surname : 'Watkins',
+//     born : 'Canada',
+//     skin : 'white'
+// }
+
+
+// // - автомобіль
+// let car = {
+//     Name : 'Porsche',
+//     Model : 'Cayenne',
+//     Volume : 3.5,
+//     Color : 'Grey',
+//     Type : 'Crossover'
+// }
+
+
+// // - квартиру
+// let flat = {
+//     location : 'City-center',
+//     Price : 46000,
+//     Rooms : 'Two-rooms',
+//     Type : 'New-building',
+//     Status : 'Sold', 
+// }
+
+
+// // - книгу
+// let book = {
+//     Author : 'Mark-Twain',
+//     Name : 'Tom-Soier',
+//     Price : 210,
+//     Pages : 345,
+//     Status : 'New'
+// }
+
+
+
+
+
+// // -- Створити масив та вивести його в консоль:
+// // - з 5 собак
+// let dogs = [
+//     {
+//         Name : 'Cesar',
+//         age : 4,
+//         color : 'Brown',
+//         breed : 'Buldog',
+//         size : 'Big' 
+//     },
+//     {
+//         Name : 'Jerry',
+//         age : 2,
+//         color : 'Grey',
+//         breed : 'Pug',
+//         size : 'small' 
+//     },  
+//     {   
+//         Name : 'Alphred',
+//         age : 3,
+//         color : 'Black',
+//         breed : 'Buldog',
+//         size : 'Big' 
+//     },  
+//     {   
+//         Name : 'Alphred',
+//         age : 3,
+//         color : 'Black',
+//         breed : 'Buldog',
+//         size : 'Big' 
+//     },  
+//     {   
+//         Name : 'Johny',
+//         age : 1,
+//         color : 'White',
+//         breed : 'Maltese',
+//         size : 'small' 
+//     },  
+//     {   
+//         Name : 'Jack',
+//         age : 5,
+//         color : 'Brown',
+//         breed : 'Chihuahua',
+//         size : 'small' 
+//     }
+// ]
+// console.log(dogs);
+
+
+
+
+// // - 3 5 людей
+// let people = [ 
+//     {
+//         Name : 'Alex',
+//         age : 25,
+//         Surname : 'Watkins',
+//         born : 'Canada',
+//         skin : 'white'
+//     },
+//     {   
+//         Name : 'Mark', 
+//         age : 49,
+//         Surname : 'Swift',
+//         born : 'USA',
+//         skin : 'Black'
+//     },
+//     {
+//         Name : 'Bred',
+//         age : 20,
+//         Surname : 'Wilshere',
+//         born : 'England',
+//         skin : 'white'
+//     },
+//     {
+//         Name : 'Danny',
+//         age : 33,
+//         Surname : 'Walback',
+//         born : 'Liverpool',
+//         skin : 'black'
+//     },
+//     {
+//         Name : 'Gareth',
+//         age : 29,
+//         Surname : 'Bale',
+//         born : 'Wales',
+//         skin : 'white'
+//     },
+// ]
+// console.log(people);
+
+
+
+
+// // - з 5 автомобілів
+// let cars = [
+//     {
+//         Name : 'Porsche',
+//         Model : 'Cayenne',
+//         Volume : 3.5,
+//         Color : 'Grey',
+//         Type : 'Crossover'
+//     },
+//     {
+//         Name : 'Kia',
+//         Model : 'Sorento',
+//         Volume : 3,
+//         Color : 'Black',
+//         Type : 'Crossover'
+//     },
+//     {
+//         Name : 'Skoda',
+//         Model : 'Superb',
+//         Volume : 2,
+//         Color : 'Blue',
+//         Type : 'Sedan'
+//     },
+//     {
+//         Name : 'Porsche',
+//         Model : 'Panamera',
+//         Volume : 3,
+//         Color : 'Yellow',
+//         Type : 'Sedan'
+//     },
+//     {
+//         Name : 'Ford',
+//         Model : 'Fusion',
+//         Volume : 2.5,
+//         Color : 'Black',
+//         Type : 'Sedan'
+//     },
+// ]
+// console.log(cars);
+
+
+
+// -- створити об'єкт (не меньше 5ти властивостей) який описує, одна з властивостей обов'язково повинна бути об'єктом,ще одна - масивом
+// - будинок
+// let building = {
+//         location : 'City-center', 
+//         age : 5,
+//         type : 'New' ,
+//         color : {frontside : 'green', backside : 'yellow' },
+//         status : 'true'
+//     }
+// // - водій
+// let driver = {
+//         name : {firstname : 'Jack', lastname : 'Wlishere', surname : 'Watkins' },
+//         status : active,
+//         age : 25,
+//         cars : [DAF, IVECO, JMC ],
+//         salary : 2000
+//     }
+// // - іграшку
+// let toy = {
+//         name : 'Teddy',
+//         price : 250,
+//         status : 'in_stock',
+//         sizes : {big : '210cm', small : '80cm' },
+//         type : 'soft_toy'
+//     }
+// // - стіл
+// let table = {
+//         types : ['computer_table', 'kitchen_table', 'writing_table'],
+//         status : 'in_stock',
+//         price : 2100,
+//         colors : {white : 'beech_tree', brown: 'oak_tree' },
+//         sizes : ['big', 'small']
+//     }
+
+
+
+
+
+
+// Дан массив:
+// let users = [
+// 				{name: 'vasya', age: 31, status: false},
+// 				{name: 'petya', age: 30, status: true},
+// 				{name: 'kolya', age: 29, status: true},
+// 				{name: 'olya', age: 28, status: false},
+// 				{name: 'max', age: 30, status: true},
+// 				{name: 'anya', age: 31, status: false},
+// 				{name: 'oleg', age: 28, status: false},
+// 				{name: 'andrey', age: 29, status: true},
+// 				{name: 'masha', age: 30, status: true},
+// 				{name: 'olya', age: 31, status: false},
+// 				{name: 'max', age: 31, status: true}
+// 			];
+// // - звернутися в відповідну ячейку масиву і відповідний параметр об'єкта і вивести в консольх наступне
+// // - статус Андрія
+// console.log(users [7].status);
+
+
+// // - статус Максима
+// console.log(users [4].status);
+
+
+// // - ім'я передостаннього об'єкту
+// console.log(users [10].name);
+
+
+// // - ім'я третього об'єкта
+// console.log(users [2].name);
+
+
+// // - вік Олега
+// console.log(users [6].age);
+
+
+// // - вік Олі
+// console.log(users [9].age);
+
+
+// // - вік + ім'я 5го об'єкта
+// console.log(users [4].age, users [4].name);
+
+
+// // - вік + сатус Анни
+// console.log(users [5].age, users[5].status);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ templatescript.js
@@ -0,0 +1,48 @@
+// -- Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
+
+// - отримує текст з параграфа з id "content"
+// let text = document.getElementById("content");
+// console.log(text);
+
+
+// - отримує текст з блоку з id "rules"
+// let text = document.getElementById("rules");
+// console.log(text);
+
+
+
+// - замініть текст параграфа з id 'content' на будь-який інший
+// let text = document.getElementById("content");
+// text.innerText = 'New text';
+// console.log(text);
+
+
+
+// - замініть текст параграфа з id 'rules' на будь-який інший
+// let text = document.getElementById("rules");
+// text.innerText = 'New text in rules';
+// console.log(text);
+
+
+
+// - змініть кожному елементу колір фону на червоний
+// document.body.style.background = 'red';
+
+
+// // - змініть кожному елементу колір тексту на синій
+// document.body.style.color = 'blue';
+
+
+
+
+// - отримати всі елементи з класом fc_rules
+// let text = document.getElementsByTagName('ul');
+// console.log(text);
+
+
+
+
+// - поміняти колір тексту у всіх елементів fc_rules на червоний
+document.ul.style.color = 'red';
+
+
